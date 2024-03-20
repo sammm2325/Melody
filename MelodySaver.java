cdd1a70aad5a72bd5249d37b2c89e5f2af83245b
Feldman <feldmans@anl.gov>
2024-03-20 13:35:31 -0500

    finished Melody project

--- MelodySaver.java
+++ MelodySaver.java
@@ -1,12 +1,16 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
 public class MelodySaver {
-    private static final String FILENAME = "melodies.txt";
+    private static final String DIRECTORY = System.getProperty("user.home") + "/Downloads/melody_files";
+    private static final String FILENAME = DIRECTORY + "/melodies.txt";
 
     public void saveMelodies(List<Melody> melodies) {
         try {
+            // Create the directory if it doesn't exist
+            new File(DIRECTORY).mkdirs();
             FileWriter writer = new FileWriter(FILENAME, false); // Overwrite existing content
             for (Melody melody : melodies) {
                 writer.write(melodyToString(melody) + "\n");
